70f8d6eeacc01295f2a8fe51e82b5e4e7a01722c
prithviraj2002 <prithvirajatul.20.cs@iite.indusuni.ac.in>
2024-03-18 22:40:22 +0530

    Colors updated

--- lib/constants/app-ui/placed_colors.dart
+++ lib/constants/app-ui/placed_colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class PlacedColors{
 
   static List<Color> gradiantColor = [Color(0XFF2D64FA), Color(0XFF43D2DF)];
+  static List<Color> disabledGradiantColor = [Color(0XFF2D64FA).withOpacity(0.4), Color(0XFF43D2DF).withOpacity(0.4)];
   static Color greyColor = Color(0xFF6C6C6C);
   static Color bgColor = Color(0xFFF1F5FA);
   static Color textColor = Color(0xFF0B2A7F);
@@ -11,19 +12,28 @@ class PlacedColors{
 
   static Color PrimaryBlack = Color(0xFF1C1C1C);
   static Color PrimaryGrey1 = Color(0xFF494949);
-  static Color PrimaryGrey2 = Color(0xFF1C1C1C).withOpacity(0.4);
-  static Color PrimaryGrey3 = Color(0xFF1C1C1C).withOpacity(0.2);
-  static Color PrimaryGrey4 = Color(0xFF1C1C1C).withOpacity(0.1);
+  // static Color PrimaryGrey2 = Color(0xFF1C1C1C).withOpacity(0.4);
+  static Color PrimaryGrey2 = Color(0XFF6C6C6C);
+  // static Color PrimaryGrey3 = Color(0xFF1C1C1C).withOpacity(0.2);
+  static Color PrimaryGrey3 = Color(0XFFAFAFAF);
+  // static Color PrimaryGrey4 = Color(0xFF1C1C1C).withOpacity(0.1);
+  static Color PrimaryGrey4 = Color(0XFFC4C4C4);
   static Color PrimaryBlueMain = Color(0xFF3269FF);
   static Color PrimaryBlueLight = Color(0xFF00B6F3);
-  static Color PrimaryBlueDark = Color(0xFFE5ECF6);
-  static Color PrimaryBlueLight1 = Color(0xFFF1F5FA);
-  static Color PrimaryBlueLight2 = Color(0xFFF7F9FB);
+  // static Color PrimaryBlueDark = Color(0xFFE5ECF6);
+  static Color PrimaryBlueDark = Color(0XFF0B2A7F);
+  // static Color PrimaryBlueLight1 = Color(0xFFF1F5FA);
+  static Color PrimaryBlueLight1 = Color(0xFFE5ECF6);
+  static Color PrimaryBlueLight2 = Color(0xFFF1F5FA);
   static Color PrimaryWhite = Color(0xFFFFFFFF);
-  static Color PrimaryOffWhite = Color(0xFFFFFFFF);
+  static Color PrimaryOffWhite = Color(0xFFF7F9FB);
 
-  static Color SecondaryGreen = Color(0xFFA1E3CB);
-  static Color SecondaryYellow = Color(0xFFFAC73C);
-  static Color SecondaryOrange = Color(0xFFFF7F09);
+  static Color SecondaryGreen = Color(0xFF00A669);
+  static Color SecondaryGreen2 = Color(0xFF39B070);
+  static Color SecondaryGreen3 = Color(0xFFA1E3CB);
+  // static Color SecondaryYellow = Color(0xFFFAC73C);
+  static Color SecondaryYello = Color(0xFFFFE999);
+  // static Color SecondaryOrange = Color(0xFFFF7F09);
+  static Color SecondaryOrange = Color(0xFFFFCB83);
   static Color SecondaryRed = Color(0xFFFF4747);
 }
